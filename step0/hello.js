89a44dc48cd547817a4da58169aadadebb2b90f8
Sofía Martín Pajares <sofia.martin-pajares@epitech.eu>
2024-01-31 17:05:17 +0100

    step0

--- step0/hello.js
+++ step0/hello.js
@@ -6,9 +6,9 @@ const port = 8000;
 
 // Create HTTP server, fill the comments with your code
 const server = http.createServer(function (req, res) {
-  // Set the response HTTP header with HTTP status and Content type
-
-  // Send the response body "Hello World"
+	res.statusCode = 200;
+	res.setHeader("Content-Type", "text/plain");
+	res.end("Hello World");
 });
 
 // Prints a log once the server starts listening
